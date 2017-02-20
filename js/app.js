a801aed04e002eb3255b1105dfda298b9761812c
Sean Lawrenz <seanlaw27@gmail.com>
2017-02-20 10:58:42 -0500

    added local JSON functionality

--- js/app.js
+++ js/app.js
@@ -1,46 +1,33 @@
-var htmlBuilder = function(obj){
-	$.each(obj, function(i, tasks){
-		html = '<li>' + tasks.name + '<span class="date"> ' + tasks.date + '</span><span class="assigned"> ' + tasks.assigned + '</span></li>';	
-		$('#tasks').append(html);	
-	});
+var listart = '<li>', lisdate = '<span class="date">', liassigned = '</span> <span class="assigned">', liend = '</span></li>';
+
+//JSON 
+function loadTasks() {
+  $.getJSON("js/tasks.json", function(result) {
+    $.each(result, function(i, task) {
+      $("#tasks").append(listart + task.name + lisdate + task.date + liassigned + task.assigned + liend);
+    });
+  });
 }
 
-$(function(){
- 
-var tasks =	'[ '+
-				'{"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },'+
-				'{"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }'+
-			']';
+function Tasks(name,date,assigned){
+	this.name = name;
+	this.date = date;
+	this.assigned = assigned;
+}
 
-var obj = $.parseJSON(tasks);
+Tasks.prototype.addTask = function(){
+	$('#tasks').append(listart + this.name + lisdate + this.date + liassigned + this.assigned + liend);
+}
 
-htmlBuilder(obj);
-
-//Add data to JSON via stringify, 
-//It's a little clumsy because I have to empty the data and refill, but I cannot use a web server 
 $('#newTask').submit(function(e){
 	e.preventDefault();
 	var name = $('#name').val();
 	var date = $('#date').val();
-	var asgd = $('#assigned').val();
-		if(name != '' && date != ''&& asgd !=''){
-			obj.push({"name": name, "date": date,"assigned":asgd});
-			tasks= JSON.stringify(obj);
-			$('#tasks').empty();
-			htmlBuilder(obj);
-			$('#name').val('');
-			$('#date').val('');
-			$('#assigned').val('');	
-		}else{
-			//I know this is generic 
-			alert('Please fill out all forms');
-		}
+	var assigned = $('#assigned').val();
+	var newTask = new Tasks(name, date, assigned);
+	newTask.addTask();
 });
 
-
-});
+$(function(){
+	loadTasks();
+});	
