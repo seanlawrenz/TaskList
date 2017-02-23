82bc9483ab20b9a2b88f1bf6470306b11c9510e8
Sean Lawrenz <seanlaw27@gmail.com>
2017-02-22 19:15:09 -0500

    tired going to bed

--- js/app.js
+++ js/app.js
@@ -19,12 +19,29 @@ Tasks.prototype.addTask = function(){
 	$('#tasks').append(listart + this.name + lisdate + this.date + liassigned + this.assigned + liend);
 }
 
+//Form vaildation
+function formVailidation(expression, regex){
+	return regex.test(expression);
+}
+
 $('#newTask').submit(function(e){
 	e.preventDefault();
+	//Regex
+	var lettersNumbersReg = new RegExp("^[A-Z0-9 _]*[A-Z0-9][A-Z0-9 _]*$");
+	var dateReg = new RegExp("^\d{1,2}\/\d{1,2}\/\d{4}$");
+	var nameReg = new RegExp("/^[a-zA-Z\s]*$/;");
+	//values
 	var name = $('#name').val();
 	var date = $('#date').val();
 	var assigned = $('#assigned').val();
+	if(formVailidation(name, lettersNumbersReg) === false){
+		
+	}
+
 	var newTask = new Tasks(name, date, assigned);
+	//vaildation
+
+
 	newTask.addTask();
 });
 
