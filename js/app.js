eb6b714fe0a797b622eeb6690600a62089280b37
Sean Lawrenz <seanlaw27@gmail.com>
2017-02-23 18:13:45 -0500

    styles and form validation done

--- js/app.js
+++ js/app.js
@@ -27,22 +27,22 @@ function formVailidation(expression, regex){
 $('#newTask').submit(function(e){
 	e.preventDefault();
 	//Regex
-	var lettersNumbersReg = new RegExp("^[A-Z0-9 _]*[A-Z0-9][A-Z0-9 _]*$");
-	var dateReg = new RegExp("^\d{1,2}\/\d{1,2}\/\d{4}$");
-	var nameReg = new RegExp("/^[a-zA-Z\s]*$/;");
+	var lettersNumbersReg = new RegExp(/^[A-Za-z0-9!@#$%^&*()_ ]{1,40}$/);
+	var dateReg = new RegExp(/^\d{1,2}\/\d{1,2}\/\d{4}$/);
+	var nameReg = new RegExp(/^[A-Za-z0-9 ]{3,20}$/);
 	//values
 	var name = $('#name').val();
 	var date = $('#date').val();
 	var assigned = $('#assigned').val();
-	if(formVailidation(name, lettersNumbersReg) === false){
-		
-	}
-
-	var newTask = new Tasks(name, date, assigned);
 	//vaildation
-
-
-	newTask.addTask();
+	if ((name == '') || (date == '') || (assigned == '')){
+		alert('Please fill out all forms');
+	}else if((formVailidation(name, lettersNumbersReg) === false) || (formVailidation(date,dateReg) === false) || (formVailidation(assigned,nameReg) === false)){
+			alert('Please check all forms again');
+	}else{
+		var newTask = new Tasks(name, date, assigned);
+		newTask.addTask();
+	}	
 });
 
 $(function(){
